3518146d8838fa65234022e1af99108f6dbde535
sania <sshaikh23464@gmail.com>
2025-08-14 15:03:55 +0530

    Add files via upload

---
+++ sania/1.4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{    int num, rev = 0, rem;
+
+    printf("enter a numbers: ");
+    scanf("%d", &num);
+
+    while (num !=0){
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num /= 10;
+    }
+
+    printf("reversed numbers = %d", rev);
+
+    return 0;
+
+}
